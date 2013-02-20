fe6054ca24d8e8691d35862ef5e143d88c053c2e
Dylan Black <dblack12705@gmail.com>
2013-02-20 00:58:58 -0800

    scholarship page
    i distilled everything on the scholarship page on ucla's website into
    this picture. it's not very well done, but it's not too bad. i don't
    know if you'd like this sort of thing. what do you think?

--- about/scholarship.php
+++ about/scholarship.php
@@ -7,6 +7,7 @@
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
       <div class="main clearfix">
+	  <div class="cont solo"><img src="aboutrsp.png"></div>
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
