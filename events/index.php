b2d617d63f7fb581e472c02955fda6aa3e53eedd
Keaton Boyle <keatonboyle@gmail.com>
2013-03-16 00:35:03 -0700

    basic scrolling grid

--- events/index.php
+++ events/index.php
@@ -3,12 +3,24 @@
 <head>
    <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/head.php"); ?>
 </head>
+<link href="calendar_style.css" rel="stylesheet" type="text/css"/>
 <body>
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
       <div class="main clearfix">
+
+<div id="left" onclick="slide_cal_left()">left</div>
+<div id="right" onclick="slide_cal_right()">right</div>
+
+<div id="calendar_canvas">
+</div>
+        This is a calendar.
+
+
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
+   
+<script type="text/javascript" charset="utf-8" src="calendar.js"></script>
 </body>
 </html>
