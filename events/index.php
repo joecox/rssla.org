e160e8272864cd774769854b2a402dce81e74b87
Keaton Boyle <keatonboyle@gmail.com>
2013-03-22 00:12:47 -0700

    working, with first line date numbers

--- events/index.php
+++ events/index.php
@@ -3,24 +3,30 @@
 <head>
    <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/head.php"); ?>
 </head>
-<link href="calendar_style.css" rel="stylesheet" type="text/css"/>
+<link href="/_layout/stylesheets/events_styles.css" rel="stylesheet" type="text/css"/>
 <body>
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
       <div class="main clearfix">
 
-<div id="left" onclick="slide_cal_left()">left</div>
-<div id="right" onclick="slide_cal_right()">right</div>
-
-<div id="calendar_canvas">
-</div>
-        This is a calendar.
+        <div id="cal_nav_wrapper">
+          <div class="cal_nav_button">
+            <img src="/resources/images/UI/prevbutton.png"
+                 onclick="slide_cal_right()">
+          </div>
+          <div id="calendar_canvas">
+          </div>
+          <div class="cal_nav_button">
+            <img src="/resources/images/UI/nextbutton.png"
+                 onclick="slide_cal_left()">
+          </div>
+        </div>
 
 
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
    
-<script type="text/javascript" charset="utf-8" src="calendar.js"></script>
+<script type="text/javascript" charset="utf-8" src="/_layout/scripts/events.js"></script>
 </body>
 </html>
