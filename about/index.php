103c0f25aa63ef29e88f92bab8060656ed0faaa3
Dylan Black <dblack12705@gmail.com>
2013-04-02 22:40:00 -0700

    WIP
    Scholarship and Index

--- about/index.php
+++ about/index.php
@@ -12,29 +12,24 @@
             <div class="bannertext"></div>
          </div>
          <div class="cont quarter left" id="sidebar">
-            <div class="pagetitle">ABOUT</div>
+            <div class="pagetitle">ABOUT US</div>
          </div>
-         <div class="cont three-quarters" style="height: 400px">
-            <div class="fillcontainer">
+         <div class="cont three-quarters" style="height: 540px">
+          <div class="fillcontainer">
                <img class="fill" src="/resources/images/society/2GM_F12.png">
-            </div>
-   			<!--
-               <div class="blocktitlecenter">Our Mission</div>
-      			<br>
-      			<div>RSSLA is a society created for those who have been awarded the Regents Scholarship, the highest award that can be bestowed on an incoming undergraduate. Its function is to:</div>
+            </div> 
+   			<div class="contentblock">
+      			<div>The Regents Scholar Society at UCLA is a academic and social society created for those who have been awarded the Regents Scholarship, the highest award that can be bestowed upon an incoming undergraduate at UCLA. Its function is to:</div>
       			<ul>
       			<li>Promote academic excellence amongst the recipients of the scholarship</li>
       			<li>Provide community service opportunities for those who wish to take advantage of them</li>
       			<li>Foster interpersonal growth amongst Regents Scholars at UCLA</li>
-      			<li>Provide resources and guidance to incoming Regents Scholars</li>
-      			<li>Promote interaction between Regents Scholars across all University of California campuses</li> 
+      			<li>Offer resources and guidance to incoming Regents Scholars</li>
+      			<li>Facilitate interaction between Regents Scholars across all University of California campuses</li> 
       			</ul>
-      			All current Regents Scholars at UCLA are eligible for membership in the society, and there are no entrance requirements save the receipt of the scholarship. Contact <?php $pres_FL ?> PHPCODE for further information?
-      			<div class="blocktitlecenter">The Three Pillars (not final graphic)</div>
-      			
-      			<b>notes to self-use whitespace in image - pointy top does not go on middle of page, etc.</b>
-               <img src="pillars.png">
-            -->
+      			All current Regents Scholars at UCLA are eligible for membership in the society, and there are no entrance requirements save the receipt of the scholarship.
+      		</div>
+            
          </div>
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
--- about/scholarship.php
+++ about/scholarship.php
@@ -7,7 +7,36 @@
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
       <div class="main clearfix">
-	  <div class="cont solo"><img src="aboutrsp.png"></div>
+         <div class="bannerwrap">
+            <div class="bannertext"></div>
+         </div>
+         <div class="cont quarter left" id="sidebar">
+            <div class="pagetitle">THE SCHOLARSHIP</div>
+         </div>
+         <div class="cont three-quarters" style="height: 480px">
+   			<div class="contentblock">
+				The top 1.5% of freshman applicants to UCLA, along with approxmately 
+				600 third year transfer students are invited to apply for the Regents 
+				Scholarship. Of these students, 100 students receive the offer of 
+				scholarship. <br><br>
+				The application itself is separate from the UC application, and 
+				consists of two short personal essays and a letter of recommendation. 
+				The application review and selection process is conducted by the faculty advisors to the Regents
+				Scholar Society.
+				<br> <br> <br>
+				The Scholarship itself consists of:
+				<ul>
+				<li>A $2000 honorarium, provided <i>ex gratia</i> as well as an 
+				amount of financial aid sufficient to meet the needs of the scholar 
+				<li>Preferential pre-enrollment in UCLA courses
+				<li>Guaranteed on-campus housing for four years
+				<li>Guaranteed on-campus parking
+				<li>Eligibility to join the Regents Scholar Society
+				</ul>
+				<br><br>
+				However, the recipient of the scholarship must maintain a cumulative GPA at or above 3.0 in order to continue to receive the benefits of the scholarship.
+            </div>
+         </div>
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
