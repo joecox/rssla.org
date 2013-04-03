cb8a331b81999a77599b0ea0456877f6a5cdac77
Dylan Black <dblack12705@gmail.com>
2013-04-02 22:59:05 -0700

    LAST WIP for the night
    avery is still helping

--- about/leadership.php
+++ about/leadership.php
@@ -7,6 +7,10 @@
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
       <div class="main clearfix">
+		<div class="cont solo" id="sidebar">
+		COMING WHEN DYLAN GETS HIS SHIT TOGETHER
+		</div>
+
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
