a0ba16aac3856957d848cff4fb8afda1f5c6e41b
Dylan Black <dblack12705@gmail.com>
2013-04-02 23:42:31 -0700

    Okay i lied
    This is just the layout that avery and I want to use for the leadership
    page, maybe put a border around the images, standardize their size, and
    use better formatting to put the blurbs and he images side by side and
    sized correctly. also these are not the final images we will be using.

--- about/leadership.php
+++ about/leadership.php
@@ -6,9 +6,27 @@
 <body>
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
-      <div class="main clearfix">
-		<div class="cont solo" id="sidebar">
-		COMING WHEN DYLAN GETS HIS SHIT TOGETHER
+      <div class="main clearfix">THIS WILL BE RELEASED WHEN DYLAN GETS HIS SHIT TOGETHER
+	  
+		<div class="cont quarter left" id="sidebar">
+		
+		<br><div class="contentblock"><img src="/resources/images/allen.jpg" width="200em" height="auto"></div>
+		<div class="contentblock"><img src="/resources/images/christina.jpg" width="200em" height="auto"></div>
+		</div>
+		<div class="cont three-quarters" style="height: 540px">
+		<div class="contentblock">
+		<b><i>Our Illustrious President:</b></i> <br>"Simplicity is the ultimate sophistication." - Leonardo da Vinci<br>Allen Huang is a fourth-year Electrical Engineering major with a Philosophy minor. As president, Allen serves as the face of the society, presiding over all RSS meetings and activities and providing guidance to its members. In previous years, Allen has also served as 
+the society's Outreach Director. Allen enjoys running, playing ultimate frisbee, and socializing with his fellow Regents Scholars in his free time. 
+Allen is always open to adventure, call him toll free at <br><strong>(858) 775-9621</strong> for an unforgettable experience! 
+		</div>
+		
+		
+		<div class="contentblock">
+		<br><br><br><br><b><i>Our Scintillating Internal Vice President:</b></i> <br>Christina Chung is a third year Neuroscience major with a Philosophy minor. As the IVP, Christina organizes and directs the logistical preparation for the Society's 
+bi-quarterly Ge1neral Meetings and assists Allen in monitoring the progress of the other officers and committees. Christina aims to eventually become a practicing physician and has been involved 
+in both volunteer work at the Santa Monica Hospital and research at the Brain Research Center. In her spare time, Christina enjoys baking, running, and bonding with RSS members.
+		</div>
+		
 		</div>
 
       </div>
