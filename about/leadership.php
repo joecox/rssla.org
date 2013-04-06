2ea9abc5300668757ef13d63aa7c3f7ae77929ba
Dylan Black <dblack12705@gmail.com>
2013-04-05 21:23:47 -0700

    howdy

--- about/leadership.php
+++ about/leadership.php
@@ -6,30 +6,54 @@
 <body>
    <div id="wrapper">
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
-      <div class="main clearfix">THIS WILL BE RELEASED WHEN DYLAN GETS HIS SHIT TOGETHER
-	  
-		<div class="cont quarter left" id="sidebar">
-		
-		<br><div class="contentblock"><img src="/resources/images/allen.jpg" width="200em" height="auto"></div>
-		<div class="contentblock"><img src="/resources/images/christina.jpg" width="200em" height="auto"></div>
-		</div>
-		<div class="cont three-quarters" style="height: 540px">
-		<div class="contentblock">
-		<b><i>Our Illustrious President:</b></i> <br>"Simplicity is the ultimate sophistication." - Leonardo da Vinci<br>Allen Huang is a fourth-year Electrical Engineering major with a Philosophy minor. As president, Allen serves as the face of the society, presiding over all RSS meetings and activities and providing guidance to its members. In previous years, Allen has also served as 
-the society's Outreach Director. Allen enjoys running, playing ultimate frisbee, and socializing with his fellow Regents Scholars in his free time. 
-Allen is always open to adventure, call him toll free at <br><strong>(858) 775-9621</strong> for an unforgettable experience! 
-		</div>
-		
-		
-		<div class="contentblock">
-		<br><br><br><br><b><i>Our Scintillating Internal Vice President:</b></i> <br>Christina Chung is a third year Neuroscience major with a Philosophy minor. As the IVP, Christina organizes and directs the logistical preparation for the Society's 
-bi-quarterly Ge1neral Meetings and assists Allen in monitoring the progress of the other officers and committees. Christina aims to eventually become a practicing physician and has been involved 
-in both volunteer work at the Santa Monica Hospital and research at the Brain Research Center. In her spare time, Christina enjoys baking, running, and bonding with RSS members.
-		</div>
-		
-		</div>
+      <div align="center" class="pagetitle"> Executive Board of RSS </div>
+			<div class="main clearfix">
+				<div class="cont quarter left" id="sidebar">	
+					<div class="aboutblock"><img src="/resources/images/allen.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/christina.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/gayan.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/keaton.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/emily.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/anna.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/sedina.jpg" id="aboutimage"></div>
+				</div>
+				<div class="cont three-quarters" style="height: 540px">
+					<div class="aboutblock">
+					<b><i>Our President:</b></i><br>
+					"Bisexuality immediately doubles your chances for a date on Saturday night." -Allen Huang<br><br>
+					Allen Huang is a fourth-year Electrical Engineering major with a Philosophy minor. 
+					As president, Allen serves as the face of the society, presiding over all RSS meetings and activities and providing guidance to its members. 
+					In previous years, Allen has also served as the society's Outreach Director. 
+					He enjoys running, playing ultimate frisbee, and socializing with his fellow Regents Scholars in his free time. 
+					Allen is always open to adventure, for a good time, call toll-free at<br>
+					<strong>(858) 775-9621</strong>!</div>
+					<div class="aboutblock"><b><i>Our Internal Vice President:</b></i><br>
+					"I don't suffer from insanity, I enjoy every minute of it" -Christina Chung<br><br>
+					Christina Chung is a third year Neuroscience major with a Philosophy minor. 
+					As the IVP, Christina organizes and directs the logistical preparation for the Society's 
+					bi-quarterly General Meetings and assists Allen in monitoring the progress of the other officers and committees. 
+					Christina aims to eventually become a practicing physician, and has been involved in both volunteer work at the Santa 
+					Monica Hospital and research at the Brain Research Center. In her spare time, Christina enjoys baking, running, going AP Kog-maw and breaking the meta, and bonding with RSS members.</div>
+					<div class="aboutblock"><b><i>Our Corporate Relations Director:</b></i><br>"'Scuse me, 'scuse me, I'm-a drink a little more than I should tonight." -Gayan Seneviratna<br><br>
+					Corporate Relations Director Gayan Seneviratna is a second year cognitive science major. As the CRD, Gayan is responsible for applying for grants and sponsorships to help fund the Society's
+					activities, as well as organizing fundraisers and finding job opportunities for members from local businesses. Gayan plans on going to graduate school and eventually ending up in some
+					form of research. In his free time, Gayan enjoys spending time with his fellow Regents Scholars and eating Sunday brunches (Contact Kyle for more information on Sunday brunch).</div>
+					<div class="aboutblock"><b><i>Our Treasurer:</b></i><br>"Screw the rules, I have money!" -Keaton Boyle<br><br>Treasurer Keaton Boyle is a third year Computer Science major with a Political Science minor. As Treasurer, Keaton manages and disperses the Society's funds, compiles a quarterly financial
+					report, and files reports and tax returns for the Society with the IRS like a boss. Keaton is a Resident Assistant in Sproul Hall and a member of the Campus Events Commission this year. In his free time,
+					Keaton enjoys playing guitar, racquetball, finding creative places to sleep in his hammock, and exploring the campus at night.</div>
+					<div class="aboutblock"><b><i>Our Secretary:</b></i><br>Secretary Emily Parker is a third year Environmental Science major with a Environmental Engineering minor. As secretary, Emily [theoretically] records the minutes at Society meetings, manages its documents,
+					and sends out weekly email bulletins that everyone reads thoroughly and diligently summarizing the coming week's events. Emily also works as a New Student Adviser (NSA), helping to guide incoming UCLA freshmen of all types during their
+					orientation sessions. In her free time, Emily works for ASUCLA Catering, listens to and plays accordion music, and explores Los Angeles.</div>
+					<div class="aboutblock"><b><i>Our Activities Director:</b></i><br>Activities Director Anna Dornisch is a second year neuroscience major. As the activities director, Anna plans and organizes social events and outings for Society members, such as the
+					Fall Barbeque and Winter broomball. Anna currently works as an Emergency Medical Technician for UCLA's EMS system, and hopes to go into trauma surgery in the future. In her free time, Anna
+					enjoys horseback riding, reading, and watching TV. Side note: Anna has no pictures with our Illustrious Commander-in-Chief.</div>
+					<div class="aboutblock"><b><i>Our Publicist</b></i><br>"To err is human, to blame it on somebody else shows management potential." -Sedina Alicic<br><br>
+					Our illustrious head of the Publicity and Propaganda Department, Sedina Alicic, is responsible for controlling information flow and and out of RSS, as dictated by the Party leaders. She, along with the Agents for Enforcement of Proper Thoughts as Befitting Regents Scholars of UCLA, make sure that there are no unproductive or dissenting opinions within the entirety of RSS. Sedina, with iron will and a strength of character matched only by our Fearless Leader and Master of All He Surveys, disseminates information at incredible speed via facebook and state-of-the-art newsletter technology. In her free time, she enjoys writing for the Daily Bruin and ruthlessly crushing the numerous small rebellions that crop up periodically in the Regents Scholar Society. Respect her. Love her. Fear her. Sedina Alicic. 
+					<div class="aboutblock"><b><i>Our NEXT</b></i><br>
+				</div>1
+			</div>
 
-      </div>
+      
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
 </body>
