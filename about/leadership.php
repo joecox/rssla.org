c8ac1b569754b8ef8e9846b1550983a075841826
Dylan Black <dblack12705@gmail.com>
2013-04-08 17:51:21 -0700

    format mostly done, pictures needed

--- about/leadership.php
+++ about/leadership.php
@@ -11,46 +11,220 @@
 				<div class="cont quarter left" id="sidebar">	
 					<div class="aboutblock"><img src="/resources/images/allen.jpg" id="aboutimage"></div>
 					<div class="aboutblock"><img src="/resources/images/christina.jpg" id="aboutimage"></div>
-					<div class="aboutblock"><img src="/resources/images/gayan.jpg" id="aboutimage"></div>
-					<div class="aboutblock"><img src="/resources/images/keaton.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/catherine.jpg" id="aboutimage"></div>
 					<div class="aboutblock"><img src="/resources/images/emily.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/keaton.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/gayan.jpg" id="aboutimage"></div>
 					<div class="aboutblock"><img src="/resources/images/anna.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/aanand.jpg" id="aboutimage"></div>
 					<div class="aboutblock"><img src="/resources/images/sedina.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/jeff.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/angel.jpg" id="aboutimage"></div>
+					<div class="aboutblock"><img src="/resources/images/molly.jpg" id="aboutimage"></div>
 				</div>
 				<div class="cont three-quarters" style="height: 540px">
 					<div class="aboutblock">
-					<b><i>Our President:</b></i><br>
-					"Bisexuality immediately doubles your chances for a date on Saturday night." -Allen Huang<br><br>
-					Allen Huang is a fourth-year Electrical Engineering major with a Philosophy minor. 
-					As president, Allen serves as the face of the society, presiding over all RSS meetings and activities and providing guidance to its members. 
-					In previous years, Allen has also served as the society's Outreach Director. 
-					He enjoys running, playing ultimate frisbee, and socializing with his fellow Regents Scholars in his free time. 
-					Allen is always open to adventure, for a good time, call toll-free at<br>
-					<strong>(858) 775-9621</strong>!</div>
-					<div class="aboutblock"><b><i>Our Internal Vice President:</b></i><br>
-					"I don't suffer from insanity, I enjoy every minute of it" -Christina Chung<br><br>
-					Christina Chung is a third year Neuroscience major with a Philosophy minor. 
-					As the IVP, Christina organizes and directs the logistical preparation for the Society's 
-					bi-quarterly General Meetings and assists Allen in monitoring the progress of the other officers and committees. 
-					Christina aims to eventually become a practicing physician, and has been involved in both volunteer work at the Santa 
-					Monica Hospital and research at the Brain Research Center. In her spare time, Christina enjoys baking, running, going AP Kog-maw and breaking the meta, and bonding with RSS members.</div>
-					<div class="aboutblock"><b><i>Our Corporate Relations Director:</b></i><br>"'Scuse me, 'scuse me, I'm-a drink a little more than I should tonight." -Gayan Seneviratna<br><br>
-					Corporate Relations Director Gayan Seneviratna is a second year cognitive science major. As the CRD, Gayan is responsible for applying for grants and sponsorships to help fund the Society's
-					activities, as well as organizing fundraisers and finding job opportunities for members from local businesses. Gayan plans on going to graduate school and eventually ending up in some
-					form of research. In his free time, Gayan enjoys spending time with his fellow Regents Scholars and eating Sunday brunches (Contact Kyle for more information on Sunday brunch).</div>
-					<div class="aboutblock"><b><i>Our Treasurer:</b></i><br>"Screw the rules, I have money!" -Keaton Boyle<br><br>Treasurer Keaton Boyle is a third year Computer Science major with a Political Science minor. As Treasurer, Keaton manages and disperses the Society's funds, compiles a quarterly financial
-					report, and files reports and tax returns for the Society with the IRS like a boss. Keaton is a Resident Assistant in Sproul Hall and a member of the Campus Events Commission this year. In his free time,
-					Keaton enjoys playing guitar, racquetball, finding creative places to sleep in his hammock, and exploring the campus at night.</div>
-					<div class="aboutblock"><b><i>Our Secretary:</b></i><br>Secretary Emily Parker is a third year Environmental Science major with a Environmental Engineering minor. As secretary, Emily [theoretically] records the minutes at Society meetings, manages its documents,
-					and sends out weekly email bulletins that everyone reads thoroughly and diligently summarizing the coming week's events. Emily also works as a New Student Adviser (NSA), helping to guide incoming UCLA freshmen of all types during their
-					orientation sessions. In her free time, Emily works for ASUCLA Catering, listens to and plays accordion music, and explores Los Angeles.</div>
-					<div class="aboutblock"><b><i>Our Activities Director:</b></i><br>Activities Director Anna Dornisch is a second year neuroscience major. As the activities director, Anna plans and organizes social events and outings for Society members, such as the
-					Fall Barbeque and Winter broomball. Anna currently works as an Emergency Medical Technician for UCLA's EMS system, and hopes to go into trauma surgery in the future. In her free time, Anna
-					enjoys horseback riding, reading, and watching TV. Side note: Anna has no pictures with our Illustrious Commander-in-Chief.</div>
-					<div class="aboutblock"><b><i>Our Publicist</b></i><br>"To err is human, to blame it on somebody else shows management potential." -Sedina Alicic<br><br>
-					Our illustrious head of the Publicity and Propaganda Department, Sedina Alicic, is responsible for controlling information flow and and out of RSS, as dictated by the Party leaders. She, along with the Agents for Enforcement of Proper Thoughts as Befitting Regents Scholars of UCLA, make sure that there are no unproductive or dissenting opinions within the entirety of RSS. Sedina, with iron will and a strength of character matched only by our Fearless Leader and Master of All He Surveys, disseminates information at incredible speed via facebook and state-of-the-art newsletter technology. In her free time, she enjoys writing for the Daily Bruin and ruthlessly crushing the numerous small rebellions that crop up periodically in the Regents Scholar Society. Respect her. Love her. Fear her. Sedina Alicic. 
-					<div class="aboutblock"><b><i>Our NEXT</b></i><br>
-				</div>1
+<b><i>	The President:	</b></i><br><br>
+President Allen Huang is a fourth year Electrical Engineering major. As president, Allen serves as the face of the society, presiding over all RSS meetings and 
+activities and providing guidance to its members. In previous years, Allen has also served as the society's Outreach Director. Allen enjoys running, playing ultimate frisbee, and 
+socializing with his fellow Regents Scholars in his free time.
+
+
+<br><br>
+
+"I always think Allen looks like a muppet" -Crystal Lin
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Internal Vice President:</b></i><br><br>
+
+Internal Vice President Christina Chung is a third year Neuroscience major with a Philosophy minor. As the IVP, Christina organizes and directs the logistical preparation for the Society's 
+bi-quarterly General Meetings and assists Allen in monitoring the progress of the other officers and committees. Christina aims to eventually become a practicing physician and has been involved 
+in both volunteer work at the Santa Monica Hospital and research at the Brain Research Center. In her spare time, Christina enjoys baking, running, and bonding with RSS members.
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>	The External Vice President:	</b></i><br><br>
+
+
+
+
+
+External Vice President Katherine Chuang is a fourth year Molecular, Cell, and Developmental Biology major with a Biomedical Research minor. As the EVP, Katherine directs the Society's 
+external relations both on and off campus, and oversees RSS partnerships within the UCLA community, especially with associated faculty. In her free time, Katherine enjoys going to 
+musicals and a capella concerts, and hanging out with her fellow RSSers. Katherine will be attending the National Institutes of Health next year for a one year postbaccalaureate.
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>	The Secretary:	</b></i><br><br>
+
+
+
+Secretary Emily Parker is a third year Environmental Science major with a Environmental Engineering minor. As secretary, Emily records the minutes at Society meetings, manages its documents, 
+and sends out weekly email bulletins summarizing the coming week's events. Emily also works as a New Student Adviser (NSA), helping to guide incoming UCLA freshmen of all types during their 
+orientation sessions. In her free time, Emily works for ASUCLA Catering, listening to and playing accordion music, and exploring Los Angeles.
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Treasurer:</b></i><br><br>
+
+Treasurer Keaton Boyle is a third year Computer Science major with a Political Science minor. As treasurer, Keaton manages and disperses the Society's funds, compiles a quarterly financial 
+report, and files reports and tax returns for the Society with the IRS. Keaton is a Resident Assistant in Sproul Hall and a member of the Campus Events Commission this year. In his free time, 
+Keaton enjoys playing guitar, racquetball, finding creative places to sleep in his hammock, and exploring the campus at night.
+
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Corporate Relations Director:</b></i><br><br>
+
+
+Corporate Relations Director Gayan Seneviratna is a second year cognitive science major. As the CRD, Gayan is responsible for applying for grants and sponsorships to help fund the Society's 
+activities, as well as organizing fundraisers and finding job opportunities for members from local businesses. Gayan plans on going to graduate school and eventually ending up in some 
+form of research. In his free time, Gayan enjoys spending time with his fellow Regents Scholars and eating Sunday brunches.
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Activities Director:</b></i><br><br>
+
+Activities Director Anna Dornisch is a second year neuroscience major. As the activities director, Anna plans and organizes social events and outings for Society members, such as the 
+Fall Barbeque and Winter broomball. Anna currently works as an Emergency Medical Technician for UCLA's EMS system, and hopes to go into trauma surgery in the future. In her free time, Anna 
+enjoys horseback riding, reading, and watching TV.
+
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Educational Programs Director:</b></i><br><br>
+
+
+Educational Programs Director Aanand Patel is a fourth year Molecular, Cell, and Developmental Biology major with a Biomedical Research Minor. As the EPD, Aanand manages the Regents 
+faculty mentorship program, and plans educational events such as the dorm dinner with medical students and various career workshops. Aanand will be starting a MD-PhD program after 
+he graduates this year. In his free time, Aanand enjoys reading, hanging out with his fellow RSSers, and playing video games.
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>	The Publicity Director:	</b></i><br><br>
+
+
+
+Publicity Director Sedina Alicic is a second year Political Science and International Development Studies major. As Publicity Director, Sedina designs and sells RSS merchandise and publicizes 
+Society events. Sedina plans on attending law school in the future, and is currently involved with the UCLA Pre-Law Society. In her free time, Sedina enjoys socializing with other Regents 
+Society members and working with the Model United Nations club.
+
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The Outreach Director:</b></i><br><br>
+
+
+Outreach Director Jeff Lewis is a third year Atmospheric and Oceanic Sciences major. As Outreach Director, Jeff coordinates volunteer opportunities in the surrounding community and organizes 
+the annual Overnight Stay Program for prospective scholars. Jeff plans to attend graduate school in his field following his undergraduate education. In addition to his studies, Jeff spends 
+much of his time out hiking and backpacking in the mountains and taking photographs of the landscape as an officer of the photography club.
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>	The Alumni Relations and UC Liason:	</b></i><br><br>
+
+
+Alumni Relations and UC Liason Angel Barajas is a fourth year Neuroscience major with a Mexican Studies minor. As the ARL, Angel communicates with alumni and the other schools in the UC system, 
+and organizes the annual Regents Scholar Inter-UC Conference. Angel will be attending UC Berkeley's Optometry School this fall, and plans on eventually becoming an MD-PhD. In his free time, 
+Angel enjoys watching TV, playing sports, and hanging out with his fellow Regents Scholars.
+
+<br><br>
+
+
+</div>
+
+
+
+
+<div class="aboutblock">
+<b><i>		The LAMP Director:</b></i><br><br>
+
+LAMP Director Molly Montgomery is a third year English major with a French minor. As the LAMP Director, Molly is in charge of overseeing the Regents Scholar Society's community involvement 
+at a local high school, University High. In the future, Molly plans on pursuing a career involving writing, education, and the environment. Molly enjoys swing dancing, cooking, and creative 
+writing during her free time.
+
+<br><br>
+
+
+</div>
+
+
+
+
+
+				</div>
 			</div>
 
       
