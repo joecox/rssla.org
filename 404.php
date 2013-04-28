268548f5dd62f31869d445540529049a39cebbba
Joey Cox <communications@rssla.org>
2013-04-28 02:23:37 -0700

    S13_wk04_sun_02
    New:
    - Correct 404 handling

--- 404.php
+++ 404.php
@@ -11,6 +11,9 @@
             <img class="photobanner" src="">
             <div class="bannertext"></div>
          </div>
+         <div class="wrap border-bottom shadow-bottom">
+            <span class="pagetitle">PAGE NOT FOUND</span>
+         </div>
       </div>
       <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
    </div>
