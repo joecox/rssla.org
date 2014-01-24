98d9e638003e59e399bdbeb3f421d8132bfc8917
Joey Cox <joeyalancox@gmail.com>
2014-01-24 00:16:45 -0800

    Edit local db creds

---
+++ _layout/globals.php
@@ -0,0 +1,12 @@
+<?php
+
+   $INSTAGRAM_CLIENT_ID = '0008fc31dc8f4a0f8be9a9fbc48b3505';
+
+   /* Change these to match your local settings */
+   $DB_USER = 'root';
+   $DB_PW = '';
+   $DB_NAME = 'uclarss_db1';
+
+   $root = $_SERVER['DOCUMENT_ROOT'];
+
+?>
