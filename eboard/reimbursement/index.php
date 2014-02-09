f32ddf30a589b5d2a12221de2156f7581db2527b
Joey Cox <joeyalancox@gmail.com>
2014-02-09 14:44:16 -0800

    Reimbursement page
    And added PHPMailer

--- eboard/reimbursement/index.php
+++ eboard/reimbursement/index.php
@@ -19,7 +19,7 @@
                   <label for="phone_email">Phone or Email:</label>
                   <input type="text" name="phone_email" size="50"/>
                   <label for="subject">Committee:</label>
-                  <input type="text" name="subject" size="50"/>
+                  <input type="text" name="committee" size="50"/>
                   <label for="event_name">Name of Event:</label>
                   <input type="text" name="event_name" size="50"/>
                   <label for="event_date">Date of Event:</label>
@@ -34,71 +34,62 @@
                      </tr>
                      <tr class="event_budget_row">
                         <td>
-                           <input type="text" name="charge_1" placeholder="Ex: Food" size="40"/>
+                           <input type="text" name="budget_charge_1" placeholder="Ex: Food" size="30"/>
                         </td>
                         <td>
-                           $ <input type="text" name="price_1" placeholder="250" size="15"/>
+                           $ <input type="text" name="budget_price_1" placeholder="250" size="10"/>
                         </td>
                         <td>
-                           <input type="text" name="reason_1" placeholder="To convert to ATP for use in cellular respiration" size="50"/>
-                        </td>
-                     </tr>
-                     <tr class="event_budget_row">
-                        <td>
-                           <input type="text" name="charge_2" size="40"/>
+                           <input type="text" name="budget_reason_1" placeholder="To convert to ATP for use in cellular respiration" size="45"/>
                         </td>
                         <td>
-                           $ <input type="text" name="price_2" size="15"/>
-                        </td>
-                        <td>
-                           <input type="text" name="reason_2" size="50"/>
-                        </td>
-                     </tr>
-                     <tr class="event_budget_row">
-                        <td>
-                           <input type="text" name="charge_3" size="40"/>
-                        </td>
-                        <td>
-                           $ <input type="text" name="price_3" size="15"/>
-                        </td>
-                        <td>
-                           <input type="text" name="reason_3" size="50"/>
-                        </td>
-                     </tr>
-                     <tr class="event_budget_row">
-                        <td>
-                           <input type="text" name="charge_4" size="40"/>
-                        </td>
-                        <td>
-                           $ <input type="text" name="price_4" size="15"/>
-                        </td>
-                        <td>
-                           <input type="text" name="reason_4" size="50"/>
-                        </td>
-                     </tr>
-                     <tr class="event_budget_row">
-                        <td>
-                           <input type="text" name="charge_5" size="40"/>
-                        </td>
-                        <td>
-                           $ <input type="text" name="price_5" size="15"/>
-                        </td>
-                        <td>
-                           <input type="text" name="reason_5" size="50"/>
-                        </td>
-                     </tr>
-                     <tr class="event_budget_row">
-                        <td>
-                           <input type="text" name="charge_6" size="40"/>
-                        </td>
-                        <td>
-                           $ <input type="text" name="price_6" size="15"/>
-                        </td>
-                        <td>
-                           <input type="text" name="reason_6" size="50"/>
+                           <div class="addRow-button"></div>
                         </td>
                      </tr>
                   </table>
+                  <p><b>Reimbursement Details</b></p>
+                  <p>Please fill this out with the required information.</p>
+                  <table id="payees_table">
+                     <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Payees</th>
+                        <th>Reason for Use:</th>
+                     </tr>
+                     <tr class="payees_row">
+                        <td>
+                           <input type="text" name="payees_item_1" size="25"/>
+                        </td>
+                        <td>
+                           $ <input type="text" name="payees_price_1" size="5"/>
+                        </td>
+                        <td>
+                           <input type="text" name="payees_list_1" size="15"/>
+                        </td>
+                        <td>
+                           <input type="text" name="payees_reason_1" size="30"/>
+                        </td>
+                        <td>
+                           <div class="addRow-button"></div>
+                        </td>
+                     </tr>
+                  </table>
+                  <input type="hidden" name="event_budget_row_count" value="1"/>
+                  <input type="hidden" name="payees_row_count" value="1"/>
+
+                  <p><b>Scans</b></p>
+                  <p>Attach scans of your receipts here.</p>
+                  <table id="scans_table">
+                     <tr class="scan_row">
+                        <td>
+                           <input type="file" name="scan_1"/>
+                        </td>
+                        <td>
+                           <div class="addRow-button"></div>
+                        </td>
+                     </tr>
+                  </table>
+                  <input type="hidden" name="scans_row_count" value="1"/>
                </form>
             </div>
             <div class="inputwrap">
