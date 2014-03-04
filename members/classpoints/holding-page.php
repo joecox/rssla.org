b96e9524aeef007474cd7aa432b49c3fc899cb59
tonyalee95 <lee.tonya@yahoo.com>
2014-03-03 20:26:43 -0800

    Holding Page 3/3/14

---
+++ members/classpoints/holding-page.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/head.php"); ?>
+   <link type="text/css" rel="stylesheet" href="holdingpage.css">
+</head>
+<body>
+   <div id="wrapper">
+      <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/header.php"); ?>
+      <div class="main clearfix">
+         <div class="topbar">
+            <span class="pagetitle">Authorization Page</span>
+            <div class="topnav v-align">
+               <!-- <span class="topnavitem">Add Additional Here</span> -->
+            </div>
+         </div>
+         <div class="fullpage-contwrap">
+
+         <!-- PLACE CONTENT HERE -->
+         	<div class="block">
+				<img id="photo" src="http://rssla.org/resources/images/society/2GM_F12.png"/>
+				<div id="text">
+					<p>Submitted by:<br>Tagged:
+						<form action="">
+							<input type="checkbox" name="destination" value="Instagram">Instagram<br></input>
+							<input type="checkbox" name="destination" value="Web Banner">Web Banner</input>
+						</form>
+						<span class="button" color=000000 <a href="http://google.com">Authorize!</a></span>
+						<span class="button" color=000000 <a href="http://google.com">Remove</a></span>
+					</p>
+				</div>
+			</div>
+			<div class="block">
+				<img id="photo" src="http://static6.businessinsider.com/image/51c3212a6bb3f79c2000000f/this-space-picture-changes-our-understanding-of-how-black-holes-form.jpg"/>
+				<div id="text">
+					<p>Submitted by:<br>Tagged:
+						<form action="">
+							<input type="checkbox" name="destination" value="Instagram">Instagram<br></input>
+							<input type="checkbox" name="destination" value="Web Banner">Web Banner</input>
+						</form>
+						<span class="button" color=000000 <a href="http://google.com">Authorize!</a></span>
+						<span class="button" color=000000 <a href="http://google.com">Remove</a></span>
+					</p>
+				</div>
+			</div>
+			<div class="block">
+				<img id="photo" src="http://ewallstreeter.com/post-images/sports-central/russia-hides-olympic-rings-blunder-on-tv-broadcast.jpg"/>
+				<div id="text">
+					<p>Submitted by:<br>Tagged:
+						<form action="">
+							<input type="checkbox" name="destination" value="Instagram">Instagram<br></input>
+							<input type="checkbox" name="destination" value="Web Banner">Web Banner</input>
+						</form>
+						<span class="button" color=000000 <a href="http://google.com">Authorize!</a></span>
+						<span class="button" color=000000 <a href="http://google.com">Remove</a></span>
+					</p>
+				</div>
+			</div>
+			<div class="block">
+				<img id="photo" src="http://paperpower.co.nz/themes/theme495/img/header-image.png"/>
+				<div id="text">
+					<p>Submitted by:<br>Tagged:
+						<form action="">
+							<input type="checkbox" name="destination" value="Instagram">Instagram<br></input>
+							<input type="checkbox" name="destination" value="Web Banner">Web Banner</input>
+						</form>
+						<span class="button" color=000000 <a href="http://google.com">Authorize!</a></span>
+						<span class="button" color=000000 <a href="http://google.com">Remove</a></span>
+					</p>
+				</div>
+			</div>
+			<div class="block">
+				<img id="photo" src="http://www.cartrackgps.com/gps-tracking/producto-gps-itrackpro-person.png"/>
+				<div id="text">
+					<p>Submitted by:<br>Tagged:
+						<form action="">
+							<input type="checkbox" name="destination" value="Instagram">Instagram<br></input>
+							<input type="checkbox" name="destination" value="Web Banner">Web Banner</input>
+						</form>
+						<span class="button" color=000000 <a href="http://google.com">Authorize!</a></span>
+						<span class="button" color=000000 <a href="http://google.com">Remove</a></span>
+					</p>
+				</div>
+			</div>
+         </div>
+         <div class="bottombar">
+            <span>
+				<!--Insert bottom bar-->	
+			</span>
+         </div>
+      </div>
+      <?php include($_SERVER['DOCUMENT_ROOT']."/_layout/footer.php"); ?>
+   </div>
+</body>
+</html>
