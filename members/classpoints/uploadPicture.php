57e1f72bfbff4affd853b5d70760c2f828033a17
scytop <scytop@gmail.com>
2014-03-03 22:13:46 -0800

    created an upload function

---
+++ members/classpoints/uploadPicture.php
@@ -0,0 +1,50 @@
+<?php
+function uploadFile($firstYearCount, $secondYearCount, $thirdYearCount, $fourthYearCount)
+{
+if (!empty($_FILES['uploadedPicture'])) //be sure to name it uploadedPicture
+	{
+		$uploaddir = $_SERVER['DOCUMENT_ROOT'].'/resources/images/classpoints/';
+    	$ext = pathinfo($_FILES['uploadedPicture'], PATHINFO_EXTENSION);
+    	$query = "INSERT INTO Pictures (firstYearPoints, secondYearPoints, thirdYearPoints, fourthYearPoints) VALUES (";
+    	$query .= $firstYearCount;
+    	$query .= ", ";
+    	$query .= $secondYearCount;
+    	$query .= ", ";
+    	$query .= $thirdYearCount;
+    	$query .= ", ";
+    	$query .= $fourthYearCount;
+    	$query .= ")";
+
+		db_connect();
+		db_insert($query);
+		$id = last_insert_id();
+
+		$image_name = $id . "." . $ext;
+		$uploadfile = $uploaddir.$image_name;
+		move_uploaded_file($_FILES['uploadedPicture', $uploadfile);
+		$query = "UPDATE Pictures SET filepath = '" . $uploadfile . "' WHERE id=" . $id;
+
+		db_insert($query);
+	}
+else
+	echo "Either the database is currently full, or something went terribly wrong.  Please contact the Communication Committee Director."
+}
+
+// function generateUnique()
+// {
+//  	$query = "SELECT * FROM PICTURES WHERE UNIQUEID =";
+// 	for( $i = 0; $i < 200; $i++ ) //Don't have more than 200 images in the database, please
+// 		{
+// 			$newQuery = $query;
+// 			$newQuery .= $i;
+// 			$tempArray = db_select($newQuery);
+// 			if(count($tempArray) == 0)	//will loop through 1-200, trying to find the first unused variable.  uniqueID should be
+// 										// appended to the end of the file name so that they're all def. unique.
+// 				return $i;
+// 			else
+// 				continue;
+// 		}
+// 	echo "Either the database is currently full, or something went terribly wrong.  Please contact the Communication Committee Director.";
+// }
+//Depricated. Use "last_inserted_id" from modules instead.
+?>
